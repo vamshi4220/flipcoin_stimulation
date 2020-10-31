b4c0b0eafb93e0630286d6ad85031a5c89288c86
vamshi4220 <ud75281@gmail.com>
2020-10-31 17:12:56 +0530

    added winner

--- flip_coin.sh
+++ flip_coin.sh
@@ -33,3 +33,32 @@ done
 echo "FLIPCOUNT-"$count
 echo "HEADS-"$HEADS
 echo "TAILS-"$TAILS
+
+#UC3
+
+count=0
+HEADS=0
+TAILS=0
+
+while [[ $HEADS -lt 21 && $TAILS -lt 21 ]]
+do
+   flip=$((RANDOM%2))
+
+   if [ $flip -eq 1 ]
+   then
+      (( HEADS++ ))
+   else
+      (( TAILS++ ))
+   fi
+   ((count++))
+done
+echo "FLIPCOUNT-"$count
+echo "HEADS $HEADS times"
+echo "TAILS $TAILS times"
+
+if [ $HEADS -gt $TAILS ]
+then
+    echo " HEADS WIN"
+else
+    echo "TAILS WIN"
+fi
